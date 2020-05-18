cf564b336752d088d56cbbc96fd4dbc464271607
AK <adrian.kloc47@gmail.com>
2020-05-18 23:04:03 +0200

    calculator v.2

--- scripts.js
+++ scripts.js
@@ -6,11 +6,20 @@ const cBtn = document.querySelector('.c-button');
 const backBtn = document.querySelector('.backspace-button');
 const dotBtn = document.querySelector('.dot-button');
 const evalBtn = document.querySelector('.eval-button');
+const memRecBtn = document.querySelector('.memRec-button');
+const memReadBtn = document.querySelector('.memRead-button');
+const rootBtn = document.querySelector('.root-button');
+const powBtn = document.querySelector('.power-button');
+const logBtn = document.querySelector('.log-button')
 
 const displayCurr = document.querySelector('.current');
 const displayPrev = document.querySelector('.prev');
 
 let solved = false;
+let memory = {
+    value: "0",
+    empty: true,
+};
 
 numBtns.forEach((item, index) => {
     item.addEventListener('click', e =>{
@@ -49,6 +58,26 @@ opBtns.forEach((item,index) =>{
         displayPrev.innerHTML = secondNum.join("");
     })
 })
+rootBtn.addEventListener('click', e => {
+    let display = parseFloat(displayCurr.innerHTML);
+    displayCurr.innerHTML = Math.sqrt(display).toString();
+})
+powBtn.addEventListener('click', e => {
+    let display = parseFloat(displayCurr.innerHTML);
+    displayCurr.innerHTML = Math.pow(display, 2).toString();
+})
+logBtn.addEventListener('click', e => {
+    let display = parseFloat(displayCurr.innerHTML);
+    displayCurr.innerHTML = Math.log(display).toString();
+})
+memRecBtn.addEventListener('click', e => {
+    e.preventDefault();
+    memory.value = displayCurr.innerHTML;
+    memory.empty = false;
+})
+memReadBtn.addEventListener('click', e => {
+    displayCurr.innerHTML = memory.value;
+})
 ceBtn.addEventListener('click', e => {
     e.preventDefault();
     displayCurr.innerHTML = "0";
@@ -57,6 +86,7 @@ ceBtn.addEventListener('click', e => {
 cBtn.addEventListener('click', e => {
     e.preventDefault();
     displayCurr.innerHTML = "0";
+    memory.value = "0";
 })
 backBtn.addEventListener('click', e => {
     e.preventDefault();
